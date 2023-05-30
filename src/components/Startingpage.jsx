017d3931d1c553b228841b5722cad8823dfb0504
Einikin <liefjaern@gmail.com>
2023-05-30 14:13:01 +0200

    Joda

--- src/components/Startingpage.jsx
+++ src/components/Startingpage.jsx
@@ -64,7 +64,7 @@ const Startingpage = () => {
         <Grid item>
         <Box textAlign='center'>
   <Button variant='contained'>
-     START
+     JEeee
   </Button>
 </Box>
             </Grid>
