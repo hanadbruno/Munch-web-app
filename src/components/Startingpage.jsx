391d4b6000c79072a790c0cd9241c0c6d4ab260d
Deppern <jobr029@student.kristiania.no>
2023-06-01 11:20:26 +0200

    edited drawingpage and finished starting page

--- src/components/Startingpage.jsx
+++ src/components/Startingpage.jsx
@@ -4,69 +4,23 @@ import "../App.css";
 function App() {
   const navigate = useNavigate();
 
-const useStyles = makeStyles((theme) => ({
-  startingPage: {
-    height: '100vh',
-  },
-  pageTitle: {
-    marginBottom: theme.spacing(2),
-  },
-}));
-
-const Startingpage = () => {
-  const classes = useStyles();
-  const [inputValue, setInputValue] = useState('');
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle the submitted value
-    console.log(inputValue);
-    // Reset the input field
-    setInputValue('');
+  const handleStartClick = () => {
+    navigate('/DrawingPage');
   };
 
   return (
-    <Grid
-      container
-      className={classes.startingPage}
-      direction="column"
-      justify="center"
-      alignItems="center"
-    >
-      <Grid item>
-        <Typography variant="h4" className={classes.pageTitle}>
-          MyMunch
-        </Typography>
-      </Grid>
-      <Grid item>
-        <form onSubmit={handleSubmit}>
-          <Grid container alignItems="center">
-            <Grid item>
-              <TextField
-                type="text"
-                value={inputValue}
-                onChange={handleChange}
-                placeholder="NAVN"
-                variant="outlined"
-              />
-              
-            </Grid>
-            
-          </Grid>
-        </form>
-        <Grid item>
-        <Box textAlign='center'>
-  <Button variant='contained'>
-     START
-  </Button>
-</Box>
-            </Grid>
-      </Grid>
-    </Grid>
+    <div className="container">
+      <div className="main-text">
+
+      EDVARD MUNCH TESTAMENTED A LARGE PART OF HIS WORK TO THE CITY OF OSLO.
+      THIS DONATION FORMED THE FOUNDATION FOR THE MUSEUM. HIS WORK IS OWNED BY THE PEOPLE, 
+      AND THEREFORE SO IS MUNCH. IN THIS SPIRIT OF SHARED OWNERSHIP, YOU ARE INVITED TO CONTRIBUTE YOUR ART.
+          
+      </div>
+      <button className="start-button" onClick={handleStartClick}>
+        START
+      </button>
+    </div>
   );
 }
 
