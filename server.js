d05b443bcd7941d944de54648a858d4d2e0b49af
chris <hammer.wrk@gmail.com>
2023-06-01 11:22:01 +0200

    server change

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.post('/save-image', (req, res) => {
   const data = req.body.image;
   //artist name and artwork
   const artist = "munch";
-  const artwork_name = "solstrole";
+  const artwork_name = "månestrole";
+  const uik = 123;
+  const signature_path = "hdsurivi";
+
 
   const base64Data = data.split(',')[1];
   const buffer = Buffer.from(base64Data, 'base64');
@@ -25,9 +28,11 @@ app.post('/save-image', (req, res) => {
     } else {
       // Now that the image is saved, send metadata to Python API
       axios.post('http://127.0.0.1:5000/api', {
+        uik: uik,
         artist: artist,
         artwork_name: artwork_name,
-        img: filename
+        artwork_path: filename,
+        signature_path: signature_path
       })
       .then(function (response) {
         res.send('Image and metadata saved successfully');
