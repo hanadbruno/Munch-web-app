1b4e04f7a1995b5131a7582de3f9c620dc76f776
chris <hammer.wrk@gmail.com>
2023-06-01 11:08:22 +0200

    removed drawingboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Startingpage from './components/Startingpage';
 import DrawingPage from './components/DrawingPage';
-import DrawingBoard from './components/DrawingBoard';
 
 const App = () => {
   return (
@@ -9,7 +8,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Startingpage />} />
         <Route path="/DrawingPage" element={<DrawingPage />} />
-        <Route path="/DrawingBoard"element={<DrawingBoard />} />
       </Routes>
     </Router>
   );
