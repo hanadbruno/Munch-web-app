4cd632b8fa9c463aef767919490b98ec01900f52
Deppern <jobr029@student.kristiania.no>
2023-06-01 10:00:36 +0200

    test push

--- src/components/Startingpage.jsx
+++ src/components/Startingpage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "../App.css";
 
+//TEST COMMENT
 function App() {
   const [name, setName] = useState("");
   const navigate = useNavigate();
