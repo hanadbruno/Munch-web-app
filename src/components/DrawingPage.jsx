71456bf2024da1575565b01e2838547cedacbaeb
hanadbruno <hanad1997@hotmail.no>
2023-06-06 10:32:52 +0200

    positioning of buttons, and functionality

--- src/components/DrawingPage.jsx
+++ src/components/DrawingPage.jsx
@@ -1,21 +1,21 @@
 import React, { useRef, useState } from "react";
 import "../Drawing.css";
 import CanvasDraw from "react-canvas-draw";
-// maybe remove:
 import { useNavigate } from "react-router-dom";
 import { IconButton } from "@material-ui/core";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import { Button } from "@mui/material";
 import PaletteIcon from "@mui/icons-material/Palette";
-import { Opacity } from "@mui/icons-material";
+import { Brush, Opacity } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import DeleteIcon from '@mui/icons-material/Delete';
+import BrushIcon from '@mui/icons-material/Brush';
 
 function Drawing() {
   const navigate = useNavigate();
   const [brushRadius, setBrushRadius] = useState(12);
   const [brushColor, setBrushColor] = useState("#444");
-  // canvas reference:
+  const [showSlider, setShowSlider] = useState(false);
   const canvasRef = useRef(null);
 
   const handleRadiusChange = (event) => {
@@ -40,100 +40,78 @@ function Drawing() {
     }
   };
 
-  // on save click here:
+  const handleBrushIconClick = () => {
+    setShowSlider(!showSlider);
+  };
+
+  const handleSliderChange = (event) => {
+    setBrushRadius(event.target.value);
+  };
+
   const handleSaveClick = async () => {
-    // Get the canvas' internal canvas and convert it to a base64 PNG
-    const canvas = canvasRef.current.canvasContainer.children[1];
-    const dataUrl = canvas.toDataURL("image/png");
-
-    // Send the base64 PNG to your server...
-    const response = await fetch("http://localhost:3001/save-image", {
-      // remember to specify the complete URL
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ image: dataUrl }),
-    });
-
-    if (response.ok) {
-      const { filename } = await response.json();
-      navigate("/FinishedDrawing", { state: { filename } });
-    } else {
-      // Handle error...
-    }
+    // Save functionality
   };
 
   return (
     <div className="Drawing">
       <div className="delete-icon">
-         <label>    <IconButton>
-        <DeleteIcon className="erase-button" onClick={handleEraseAll}>
-    
-        </DeleteIcon>
-        </IconButton></label>
-        </div>
+        <IconButton>
+          <DeleteIcon className="erase-button" onClick={handleEraseAll} />
+        </IconButton>
+      </div>
       <label>
-        <input
-          type="range"
-          min="1"
-          max="50"
-          value={brushRadius}
-          onChange={handleRadiusChange}
-        />
+        {showSlider && (
+          <input
+            type="range"
+            min="1"
+            max="50"
+            value={brushRadius}
+            onChange={handleSliderChange}
+          />
+        )}
        
       </label>
-      
+
       <CanvasDraw
-        ref={canvasRef}
-        className="canvas-draw"
-        loadTimeOffset={5}
-        lazyRadius={0}
-        brushRadius={brushRadius}
-        brushColor={brushColor}
-        catenaryColor={"#0a0302"}
-        gridColor={"rgba(150,150,150,0.17)"}
-        hideGrid={false}
-        canvasWidth={1000}
-        canvasHeight={1000}
-        disabled={false}
-        imgSrc={""}
-        saveData={null}
-        immediateLoading={false}
-        hideInterface={false}
-      />
+      ref={canvasRef}className="canvas-draw"
+      loadTimeOffset={5}
+      lazyRadius={0}
+      brushRadius={brushRadius}
+      brushColor={brushColor}
+      catenaryColor={"#0a0302"}
+      gridColor={"rgba(150,150,150,0.17)"}
+      hideGrid={false}canvasWidth={1000}
+      canvasHeight={1000}disabled={false}imgSrc={""}saveData={null}immediateLoading={false}hideInterface={false}/>
 
-  <label>
-
-        <div className="color-picker">
-          <div className="palette-icon">
-          <PaletteIcon fontSize="large"></PaletteIcon>
+      <label>
+        <div className="color-picker" >
+          {/* Replace color-preview with brush icon */}
+          <div className="brush-icon">
+            <PaletteIcon fontSize="large" />
           </div>
-          <div
-            className="color-preview"
-            style={{ backgroundColor: brushColor, opacity: 0}}
-          ></div>
           <input type="color" value={brushColor} onChange={handleColorChange} />
         </div>
-
+     
         <IconButton color="black" style={{ fontSize: 50 }}>
           <RefreshIcon fontSize="inherit" onClick={handleUndo} />
         </IconButton>
       </label>
+      <div className="brush-icon">
+        <IconButton onClick={handleBrushIconClick}>
+          <BrushIcon fontSize="large" />
+        </IconButton>
+        </div>
 
-      {/* save button, need to route to starting page */}
       <div className="button-container">
-        ¨
-  
         <Link to="/ExitPage">
           <button className="save-button" onClick={handleSaveClick}>
             DONE
           </button>
         </Link>
       </div>
-      
     </div>
   );
 }
 
 export default Drawing;
+
