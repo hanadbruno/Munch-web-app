ba4c86564211c8ee793d797e3a403cdd1db5c7d0
jonasgst <94687813+jonasgst@users.noreply.github.com>
2023-06-06 11:02:01 +0200

    banned words

--- server.js
+++ server.js
@@ -19,12 +19,9 @@ function generateUID() {
 
 app.post('/save-image', (req, res) => {
   const data = req.body.image;
-
-
   const base64Data = data.split(',')[1];
   const buffer = Buffer.from(base64Data, 'base64');
-
-  const timestamp = Date.now(); //image timestamp
+  const timestamp = Date.now();
   const db_artpath = `http://127.0.0.1:5000/images/artpiece_${timestamp}.jpg`
 
   filename = `C:/Users/jonas/Pictures/munch/artpiece_${timestamp}.jpg`
@@ -33,7 +30,6 @@ app.post('/save-image', (req, res) => {
       console.error(err);
       res.status(500).send('Error saving image');
     } else {
-      // Image saved, send filename back to client
       res.json({ filename: db_artpath });
     }
   });
@@ -43,21 +39,17 @@ app.post('/save-signature', (req, res) => {
   const uik = generateUID();
   const data = req.body.signature_image;
   const artworkName = req.body.artwork_name;
-  const filename = req.body.filename; // this is the artwork image filename
-
+  const filename = req.body.filename; 
   const base64Data = data.split(',')[1];
   const buffer = Buffer.from(base64Data, 'base64');
-
-  const timestamp = Date.now(); //signature timestamp
+  const timestamp = Date.now(); 
   const db_signpath = `http://127.0.0.1/images/signature_${timestamp}.jpg`
-
   const signatureFilename = `C:/Users/Jonas/Pictures/munch/signature_${timestamp}.jpg`
   fs.writeFile(signatureFilename, buffer, (err) => {
     if (err) {
       console.error(err);
       res.status(500).send('Error saving image');
     } else {
-      // Image saved, now send metadata to Python API
       axios.post('http://127.0.0.1:5000/api', {
         uik: uik,
         artwork_name: artworkName,
