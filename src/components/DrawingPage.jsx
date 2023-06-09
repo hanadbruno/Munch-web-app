761da1953293c435a1d73c90c61857766ac58a58
chris <hammer.wrk@gmail.com>
2023-06-09 09:16:20 +0200

    cleared state mem leak

--- src/components/DrawingPage.jsx
+++ src/components/DrawingPage.jsx
@@ -161,6 +161,10 @@ const handleSaveClick = async () => {
   if (response.ok) {
     const { filename, filename2 } = await response.json();
     navigate("/FinishedDrawing", { state: { filename, filename2 } });
+
+    // Clear the history state after a successful save
+    setHistory([]);
+    setStep(-1);
   } else {
     navigate("/FinishedDrawing");
   }
