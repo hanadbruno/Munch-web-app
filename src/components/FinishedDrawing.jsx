1d277a463bf0fa91c1788420eae059d0a028e880
chris <hammer.wrk@gmail.com>
2023-06-13 13:00:06 +0200

    new keyboard

--- src/components/FinishedDrawing.jsx
+++ src/components/FinishedDrawing.jsx
@@ -1,31 +1,57 @@
-import CanvasDraw from 'react-canvas-draw';
-import React, { useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import '../css/FinishedDrawing.css';
-import { bannedwords } from '../bannedwords.js';
-
+import CanvasDraw from "react-canvas-draw";
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import "../css/FinishedDrawing.css";
+import { bannedwords } from "../bannedwords.js";
 import { FaTrashAlt } from 'react-icons/fa';
-
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
 
 const FinishedDrawing = () => {
   const [brushRadius] = useState(3);
-  const [brushColor] = useState('#444');
-  const [artworkName, setArtworkName] = useState('');
+  const [brushColor] = useState("#444");
+  const [artworkName, setArtworkName] = useState("");
   const canvasRef = useRef(null);
   const navigate = useNavigate();
-
   const location = useLocation();
   const {filename, filename2} = location.state // || {};
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
+  const [layout, setLayout] = useState("default");
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+        if (keyboardVisible && !event.target.closest('.keyboard-container')) {
+            setKeyboardVisible(false);
+        }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+    };
+}, [keyboardVisible]);
   
-  const handleInputBlur = (event) => {
-    window.scrollTo(0, 0);
-  };
-  
-  const handleArtworkNameChange = (event) => {
-    document.getElementById('artwork-name').scrollIntoView();
-    setArtworkName(event.target.value);
-    window.scrollTo(0, 0);
+  function onChange(input) {
+    setArtworkName(input);
+    console.log("Input changed", input);
+  }
+
+  function onKeyPress(button){
+    console.log("Button pressed", button);
+    if (button === "{enter}") {
+      setKeyboardVisible(false);
+    }
+    if (button === "{symbols}") {
+      setLayout("symbols");
+    }
+    if (button === "{abc}") {
+      setLayout("default");
+    }
+  }
+
+  const handleInputFocus = () => {
+    setKeyboardVisible(true);
   };
 
   const handleEraseAllClick = () => {
@@ -44,7 +70,7 @@ const FinishedDrawing = () => {
   
     if (!response.ok) {
     } else {
-      navigate('/ExitPage');
+      navigate("/ExitPage");
     }
   };
 
@@ -66,62 +92,97 @@ const FinishedDrawing = () => {
         },
         body: JSON.stringify({ 
             signature_image: dataUrl,
-            artwork_name: artworkName === '' ? 'UNTITLED' : artworkName,
+            artwork_name: artworkName === "" ? "UNTITLED" : artworkName,
             filename
         })
     });
 
     if (!response.ok) {
-        navigate('/ExitPage');
+        navigate("/ExitPage");
     } else {
-        navigate('/ExitPage');
+        navigate("/ExitPage");
     }
 };
 
   return (
-    <div className='artwork-body'>
-        <div className='artwork-image'>
-          <img src={filename} alt={filename} style={{width: '100%', height: '100%'}}/>
+    <div className="ArtworkBody">
+        <div className="ArtworkImage">
+          <img src={filename} alt={filename} style={{width: "100%", height: "100%"}}/>
         </div>
-      <h3 className='title'>TITLE</h3>
-      <input 
-        id='artwork-name'
-        placeholder='UNTITLED'
-        value={artworkName}
-        onChange={handleArtworkNameChange}
-        onBlur={handleInputBlur}
-      />
-      <h3 className='title'>SIGNATURE</h3> 
-
-      <button className='erase-all-button' onClick={handleEraseAllClick}><FaTrashAlt/></button>
-
-      <CanvasDraw
-        ref={canvasRef}
-        className='canvas-draw'
-        loadTimeOffset={5}
-        lazyRadius={0}
-        brushRadius={brushRadius}
-        brushColor={brushColor}
-        catenaryColor={'#0a0302'}
-        gridColor={'rgba(150,150,150,0.17)'}
-        hideGrid={true}
-        canvasWidth={800}
-        canvasHeight={200}
-        disabled={false}
-        imgSrc={''}
-        saveData={null}
-        immediateLoading={false}
-        hideInterface={false}
-      />
-
-<div style={{ display: 'flex' }}>
-  <button className='save-button' onClick={handleSaveClick}>
-    SAVE
-  </button>
-  <button className='quit-button' onClick={handleQuit}>
-    QUIT
-  </button>
-</div>
+        <h3 className="Title">TITLE</h3>
+        <div
+          className="input-field" 
+          onClick={handleInputFocus}
+        >
+          {artworkName || "UNTITLED"} 
+        </div>
+        {!keyboardVisible && (
+          <h3 className="Title">SIGNATURE</h3>
+        )}
+        {!keyboardVisible && (
+          <button className="erase-all-button" onClick={handleEraseAllClick}><FaTrashAlt/></button>
+        )}
+        {!keyboardVisible && (
+          <CanvasDraw
+              ref={canvasRef}
+              className="canvas-draw"
+              loadTimeOffset={5}
+              lazyRadius={0}
+              brushRadius={brushRadius}
+              brushColor={brushColor}
+              catenaryColor={"#0a0302"}
+              gridColor={"rgba(150,150,150,0.17)"}
+              hideGrid={true}
+              canvasWidth={800}
+              canvasHeight={200}
+              disabled={false}
+              imgSrc={""}
+              saveData={null}
+              immediateLoading={false}
+              hideInterface={false}
+          />
+        )}
+        {!keyboardVisible && (
+          <div style={{ display: 'flex' }}>
+            <button className="save-button" onClick={handleSaveClick}>SAVE</button>
+            <button className="quit-button" onClick={handleQuit}>QUIT</button>
+          </div>
+        )}
+        {keyboardVisible && (
+          <div className="keyboard-container">
+              <Keyboard
+                  className="keyboard"
+                  onChange={onChange}
+                  onKeyPress={onKeyPress}
+                  maxLength={30}
+                  theme={"hg-theme-default darkTheme"}
+                  display={{
+                    '{bksp}': 'BACKSPACE',
+                    '{enter}': 'ENTER',
+                    '{space}': 'SPACE',
+                    '{symbols}': '.!?',
+                    '{abc}': 'ABC'
+                  }}
+                  layout={{
+                    default: [
+                      "1 2 3 4 5 6 7 8 9 0",
+                      "Q W E R T Y U I O P Å",
+                      "A S D F G H J K L Ø Æ",
+                      "Z X C V B N M {bksp}",
+                      "{symbols} {space} {enter}"
+                    ],
+                    symbols: [
+                      "- / : ; ( ) $ & @",
+                      ". , ? ! ' \" \\ `",
+                      "[ ] { } # % ^ * + =",
+                      "_ \\ | ~ < > € £ ¥ {bksp}",
+                      "{abc} {space} {enter}"
+                    ]
+                  }}
+                  layoutName={layout}
+              />
+          </div>
+        )}
     </div>
   );
 };
